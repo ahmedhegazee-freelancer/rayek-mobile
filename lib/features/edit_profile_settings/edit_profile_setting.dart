d78ae773e5199e29fc9488773f3a22d8bef1cd5f
eng-sayed <ssayedaashrf@gmail.com>
2024-05-11 11:47:50 +0300

    go back in edit profle

--- lib/features/edit_profile_settings/edit_profile_setting.dart
+++ lib/features/edit_profile_settings/edit_profile_setting.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../core/constants/dimensions.dart';
+import '../../core/router/router.dart';
 import 'widgets/body1_widget.dart';
 import 'widgets/body2_widget.dart';
 import 'widgets/body3_widget.dart';
@@ -27,38 +28,15 @@ class EditProfileSettingScreen extends StatelessWidget {
           final cubit = EditProfileSettingCubit.get(context);
           final currentIndex = cubit.currentIndex;
           return Scaffold(
-            body: NestedScrollView(
-              headerSliverBuilder:
-                  (BuildContext context, bool innerBoxIsScrolled) {
-                return <Widget>[
-                  SliverAppBar(
-                    expandedHeight: 100.h,
-                    backgroundColor: Colors.transparent,
-                    surfaceTintColor: Colors.transparent,
-                    // flexibleSpace: const FlexibleSpaceBar(
-                    //   // background: Container(
-                    //   //   height: 150.h,
-                    //   //   color: Theme.of(context).scaffoldBackgroundColor,
-                    //   //   child: Column(
-                    //   //     children: [
-                    //   //       const _Title(),
-                    //   //       const _SearchArea(),
-                    //   //       SizedBox(
-                    //   //         height: 10.h,
-                    //   //       ),
-                    //   //     ],
-                    //   //   ),
-                    //   // ),
-                    //   collapseMode: CollapseMode.parallax,
-                    // ),
-                    pinned: true,
-                    automaticallyImplyLeading: false,
-                    actions: [
-                      Padding(
-                        padding: EdgeInsets.only(
-                          left: Dimensions.defaultPadding,
-                          right: Dimensions.defaultPadding,
-                        ),
+            body: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Align(
+                        alignment: AlignmentDirectional.centerEnd,
                         child: CircleAvatar(
                           radius: 15.r,
                           backgroundColor:
@@ -69,145 +47,124 @@ class EditProfileSettingScreen extends StatelessWidget {
                                 size: 15.sp,
                                 color: ColorManager.whiteTextColor,
                                 isColored: true,
+                                isBack: false,
                                 iosOnly: true,
+                                onPressed: () {
+                                  currentIndex == 0
+                                      ? MagicRouter.goBack()
+                                      : {
+                                          cubit.changeCurrentIndex(
+                                              currentIndex - 1)
+                                        };
+                                },
                               ),
                             ),
                           ),
                         ),
                       ),
+                      SizedBox(height: 16.h),
+                      const Text(
+                        "تعديل الملف الشخصي",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xff151628)),
+                      ),
+                      SizedBox(height: 8.h),
+                      const Text(
+                        "عدل ملفك الشخصي",
+                        style: TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                            color: Color(0xff9C9C9C)),
+                      ),
+                      SizedBox(height: 16.h),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              height: 10.h,
+                              decoration: BoxDecoration(
+                                color: currentIndex == 0
+                                    ? const Color(0xff1FBAD6)
+                                    : const Color(0xffECEBEB),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8.w,
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 10.h,
+                              decoration: BoxDecoration(
+                                color: currentIndex == 1
+                                    ? const Color(0xff1FBAD6)
+                                    : const Color(0xffECEBEB),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8.w,
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 10.h,
+                              decoration: BoxDecoration(
+                                color: currentIndex == 2
+                                    ? const Color(0xff1FBAD6)
+                                    : const Color(0xffECEBEB),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8.w,
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 10.h,
+                              decoration: BoxDecoration(
+                                color: currentIndex == 3
+                                    ? const Color(0xff1FBAD6)
+                                    : const Color(0xffECEBEB),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8.w,
+                          ),
+                          Expanded(
+                            child: Container(
+                              height: 10.h,
+                              decoration: BoxDecoration(
+                                color: currentIndex == 4
+                                    ? const Color(0xff1FBAD6)
+                                    : const Color(0xffECEBEB),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            width: 8.w,
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16.h),
+                      currentIndex == 0
+                          ? const BodyOneWidget()
+                          : currentIndex == 1
+                              ? const BodyTwoWidget()
+                              : currentIndex == 2
+                                  ? const BodyThreeWidget()
+                                  : currentIndex == 3
+                                      ? const BodyFourWidget()
+                                      : const BodyFiveWidget(),
+                      SizedBox(height: 16.h),
                     ],
-                  ),
-                ];
-              },
-              body: SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        // Align(
-                        //   alignment: Alignment.centerLeft,
-                        //   child: CircleAvatar(
-                        //     radius: 15.r,
-                        //     backgroundColor:
-                        //         ColorManager.primaryColor.withOpacity(.45),
-                        //     child: Center(
-                        //       child: Center(
-                        //         child: CustomBackButton(
-                        //           size: 15.sp,
-                        //           color: ColorManager.whiteTextColor,
-                        //           isColored: true,
-                        //           iosOnly: true,
-                        //         ),
-                        //       ),
-                        //     ),
-                        //   ),
-                        // ),
-                        SizedBox(height: 16.h),
-                        const Text(
-                          "تعديل الملف الشخصي",
-                          style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.w700,
-                              color: Color(0xff151628)),
-                        ),
-                        SizedBox(height: 8.h),
-                        const Text(
-                          "عدل ملفك الشخصي",
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
-                              color: Color(0xff9C9C9C)),
-                        ),
-                        SizedBox(height: 16.h),
-                        Row(
-                          children: [
-                            Expanded(
-                              child: Container(
-                                height: 10.h,
-                                decoration: BoxDecoration(
-                                  color: currentIndex == 0
-                                      ? const Color(0xff1FBAD6)
-                                      : const Color(0xffECEBEB),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 8.w,
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: 10.h,
-                                decoration: BoxDecoration(
-                                  color: currentIndex == 1
-                                      ? const Color(0xff1FBAD6)
-                                      : const Color(0xffECEBEB),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 8.w,
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: 10.h,
-                                decoration: BoxDecoration(
-                                  color: currentIndex == 2
-                                      ? const Color(0xff1FBAD6)
-                                      : const Color(0xffECEBEB),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 8.w,
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: 10.h,
-                                decoration: BoxDecoration(
-                                  color: currentIndex == 3
-                                      ? const Color(0xff1FBAD6)
-                                      : const Color(0xffECEBEB),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 8.w,
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: 10.h,
-                                decoration: BoxDecoration(
-                                  color: currentIndex == 4
-                                      ? const Color(0xff1FBAD6)
-                                      : const Color(0xffECEBEB),
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              width: 8.w,
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 16.h),
-                        currentIndex == 0
-                            ? const BodyOneWidget()
-                            : currentIndex == 1
-                                ? const BodyTwoWidget()
-                                : currentIndex == 2
-                                    ? const BodyThreeWidget()
-                                    : currentIndex == 3
-                                        ? const BodyFourWidget()
-                                        : const BodyFiveWidget(),
-                        SizedBox(height: 16.h),
-                      ],
-                    ),
                   ),
                 ),
               ),
