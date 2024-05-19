537b7b9c8880ab67b039d4a51cf5e6f7fb8ee777
eng-sayed <ssayedaashrf@gmail.com>
2024-05-19 09:33:32 +0300

    add general cubit and video call cubit

---
+++ lib/core/general_utils/general_utils.dart
@@ -0,0 +1,8 @@
+import 'package:agora_uikit/agora_uikit.dart';
+
+import '../bloc/user_type/user_type.dart';
+
+class GeneralUtils {
+  static UserType userType = UserType.user;
+  static late AgoraClient client;
+}
